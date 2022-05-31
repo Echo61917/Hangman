da04b54b2cec2051ab4ae9a3a3ae56904196ac23
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-05-31 12:35:41 -0700

    asdf

--- src/main/java/com/example/hangman/HelloApplication.java
+++ src/main/java/com/example/hangman/HelloApplication.java
@@ -6,18 +6,51 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.TreeSet;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hangman");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+
+        TreeSet letters = new TreeSet<>();
+        letters.add("A");
+        letters.add("B");
+        letters.add("C");
+        letters.add("D");
+        letters.add("E");
+        letters.add("F");
+        letters.add("G");
+        letters.add("H");
+        letters.add("I");
+        letters.add("J");
+        letters.add("K");
+        letters.add("L");
+        letters.add("M");
+        letters.add("N");
+        letters.add("O");
+        letters.add("P");
+        letters.add("Q");
+        letters.add("R");
+        letters.add("S");
+        letters.add("T");
+        letters.add("U");
+        letters.add("V");
+        letters.add("W");
+        letters.add("X");
+        letters.add("Y");
+        letters.add("Z");
+
+
     }
+
+
 }
